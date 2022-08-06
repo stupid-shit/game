f4fd7e269d33ae5665064757f61bb593326d4b2a
ssmoogi <ssmoogi@gmail.com>
2022-08-06 14:53:21 -0700

    file upload

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div>
+          <h1>ssmoogi's about page</h1>
+          <a href="http://ssmoogi.github.io">http://ssmoogi.github.io</a>
+        </div>
+      )
+}
